8338ab214fa19d54c96643a5c2fb3330b64507b7
Andres Castellanos <andrescastellanosdelgadillo@gmail.com>
2020-07-21 16:11:34 -0500

    Router en Gatsby

---
+++ src/pages/gracias.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+import { SEO } from '../components'
+import { Button, Purchase } from '../styles/components'
+
+export default function gracias() {
+    return (
+        <div>
+            <SEO title='Compra Exitosa' />
+            <Purchase>
+                <h2>Compra Exitosa</h2>
+                <p>Espero que disfrutes tu swag, lucelo con orgullo</p>
+                <p>¡Te esperamos de vuelta, no pares de aprender!</p>
+                <span rol='img' aria-label='emoji'>❤</span>
+                <Link to=''>
+                    <Button>Volver al catálogo</Button>
+                </Link>
+            </Purchase>
+        </div>
+    )
+}
