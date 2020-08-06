1b35eb224d80f37e4102c8532f6c92d26470689f
Andres Castellanos <andres.castellanos@datia.co>
2020-08-06 16:37:52 -0500

    Terminando la Vista de Detalle de los Productos

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,6 +24,7 @@ exports.createPages = async ({ graphql, actions }) => {
                 description
                 wear
                 img
+                color
               }
             }
           }
